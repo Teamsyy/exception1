6ff1ca7fb4893479fdc3a351e38fea43f83ed15c
Teamsyy <meowmeow2346@gmail.com>
2023-03-29 16:09:14 +0700

    exception1

--- src/int103_exception_G1/Lab5.java
+++ src/int103_exception_G1/Lab5.java
@@ -1,18 +1,21 @@
-package int103_exception_G1;
-
-public class Lab5  {
+public class Lab5 {
     public static void main(String[] args) {
-       System.out.println("Main method call method doThis()");
-       try{
-           doThis();
-       } 
-       catch(Exception e){
-           System.out.println("Main method receive exception " + e.getMessage());
-       }}
-    public static void doThis() throwsException {
-        System.out.println("Method doThis() call method doThat()");doThat();
+        System.out.println("Main method call method doThis()");
+        try {
+            doThis();
+        }
+        catch (Exception e) {
+            System.out.println("Main method receive exeption" + e.getMessage());
+        }
     }
-    public static void doThat() throwsException {
-        throw new Exception();
+
+    public static void doThis() throws Exception {
+        System.out.println("Method doThis() call method doThat()");
+        doThat();
+    }
+
+
+    public static void doThat() throws Exception {
+        System.out.println(1/0);
     }
 }
